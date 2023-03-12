8356331349a6f7f8f0f0176c1969d5991223ec7f
TechnicalStudioDeveloper <appdeveloper400@gmail.com>
2023-03-12 18:50:32 +0530

    Update README : Added done mark at save and edit file. Introduction(Doc) : Added done mark at save and edit file. Updated navbar and footer New doc page : Project map

---
+++ docs/tscodeeditor/contribute/project-map.md
@@ -0,0 +1,28 @@
+---
+sidebar_position: 1
+title: Project map
+description: TS Code Editor classes map
+---
+## Activities
+- com.ts.code.editor.MainActivity
+- com.ts.code.editor.FilemanagerActivity
+- com.ts.code.editor.SettingActivity
+- com.ts.code.editor.AboutActivty
+- com.ts.code.editor.CodeEditorActivity
+- com.ts.code.editor.BrowserActivity
+
+## Classes
+- package
+	- com.ts.code.editor
+		- com.ts.code.editor.MainActivity
+		- com.ts.code.editor.FilemanagerActivity
+		- com.ts.code.editor.SettingActivity
+		- com.ts.code.editor.AboutActivty
+		- com.ts.code.editor.CodeEditorActivity
+		- com.ts.code.editor.BrowserActivity
+		- com.ts.code.editor.TSDTheme
+	- com.ts.code.editor.editor
+		- com.ts.code.editor.editor.AceEditor
+		- com.ts.code.editor.editor.Editor
+		- com.ts.code.editor.editor.SoraEditor
+		- com.ts.code.editor.editor.TSUtils
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,136 +1,141 @@
-// @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
+ // @ts-check
+ // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+ const lightCodeTheme = require('prism-react-renderer/themes/github');
+ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
-/** @type {import('@docusaurus/types').Config} */
-const config = {
-	title: 'TS Code Editor',
-	tagline: 'TS Code Editor is an Android code editor',
-	url: 'https://github.com/TS-Code-Editor',
-	baseUrl: '/',
-	onBrokenLinks: 'throw',
-	onBrokenMarkdownLinks: 'warn',
-	favicon: 'img/logo.png',
+ /** @type {import('@docusaurus/types').Config} */
+ const config = {
+ 	title: 'TS Code Editor',
+ 	tagline: 'TS Code Editor is an Android code editor',
+ 	url: 'https://github.com/TS-Code-Editor',
+ 	baseUrl: '/',
+ 	onBrokenLinks: 'throw',
+ 	onBrokenMarkdownLinks: 'warn',
+ 	favicon: 'img/logo.png',
+ 	// GitHub pages deployment config.
+ 	// If you aren't using GitHub pages, you don't need these.
+ 	organizationName: 'TS Code Editor',
+ 	// Usually your GitHub org/user name.
+ 	projectName: 'TS Code Editor',
+ 	// Usually your repo name.
 
-	// GitHub pages deployment config.
-	// If you aren't using GitHub pages, you don't need these.
-	organizationName: 'TS Code Editor', // Usually your GitHub org/user name.
-	projectName: 'TS Code Editor', // Usually your repo name.
-
-	// Even if you don't use internalization, you can use this field to set useful
-	// metadata like html lang. For example, if your site is Chinese, you may want
-	// to replace "en" with "zh-Hans".
-	i18n: {
-		defaultLocale: 'en',
-		locales: ['en'],
-	},
-
-	presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-				docs: {
-          showLastUpdateAuthor: true,
-          showLastUpdateTime: true,
-					sidebarPath: require.resolve('./sidebars.js'),
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
-					editUrl: 'https://github.com/TS-Code-Editor',
-				},
-				blog: {
-					showReadingTime: true,
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
-					editUrl: 'https://github.com/TS-Code-Editor',
-				},
-				theme: {
-					customCss: require.resolve('./src/css/custom.css'),
-				},
-			}),
-    ],
-  ],
-
-	themeConfig:
-		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-		({
-			navbar: {
-				title: 'TS Code Editor',
-				logo: {
-					alt: 'Logo',
-					src: 'img/logo.png',
-				},
-				items: [
+ 	// Even if you don't use internalization, you can use this field to set useful
+ 	// metadata like html lang. For example, if your site is Chinese, you may want
+ 	// to replace "en" with "zh-Hans".
+ 	i18n: {
+ 		defaultLocale: 'en',
+ 		locales: ['en'],
+ 	},
+ 	presets: [
+		[
+			'classic',
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+ 				docs: {
+ 					showLastUpdateAuthor: true,
+ 					showLastUpdateTime: true,
+ 					sidebarPath: require.resolve('./sidebars.js'),
+ 					// Please change this to your repo.
+ 					// Remove this to remove the "edit this page" links.
+ 					editUrl: 'https://github.com/TechnicalStudioDeveloper/TS-Code-Editor-Website',
+ 					/*lastVersion: 'current',
+ 					versions: {
+ 						current: {
+ 							label: 'Current',
+ 							path: '1.0Alpha1'
+ 						}
+ 					}*/
+ 				},
+ 				blog: {
+ 					showReadingTime: true,
+ 					// Please change this to your repo.
+ 					// Remove this to remove the "edit this page" links.
+  					editUrl: 'https://github.com/TechnicalStudioDeveloper/TS-Code-Editor-Website',
+ 				},
+ 				theme: {
+ 					customCss: require.resolve('./src/css/custom.css')
+ 				}
+ 			})]
+	],
+ 	themeConfig:
+ 		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+ 		({
+ 			navbar: {
+ 				title: 'TS Code Editor',
+ 				logo: {
+ 					alt: 'Logo',
+ 					src: 'img/logo.png',
+ 				},
+ 				items: [
+ 					{
+ 						type: 'doc',
+ 						docId: 'Introduction',
+ 						position: 'left',
+ 						label: 'Docs'
+					},
+ 					/*{
+ 						type: 'docsVersionDropdown'
+					}*/
 					{
-						type: 'doc',
-						docId: 'Introduction',
-						position: 'left',
-						label: 'Docs',
-          },
-					/*{
-						to: '/blog',
-						label: 'Blog',
-						position: 'left'
-					},*/
-					{
-						href: 'https://github.com/TS-Code-Editor',
 						label: 'GitHub',
 						position: 'right',
-          },
-        ],
-			},
-			footer: {
+						href:'https://github.com/TS-Code-Editor/TS-Code-Editor'
+					},
+					{
+						label: 'Facebook',
+						href: 'https://m.facebook.com/profile.php?id=100019763839969',
+						position: 'left'
+					},
+					{
+						label: 'Discord',
+						href: 'https://discord.gg/xG7Y6buT',
+						position: 'left'
+					}
+				]
+ 			},
+				footer: {
 				style: 'dark',
-				links: [
+					links: [
 					{
 						title: 'Docs',
 						items: [
-							{
+ 							{
 								label: 'Tutorial',
 								to: '/docs/Introduction',
-              },
-            ],
-          },
-					{
-						title: 'Community',
-						items: [
-							{
-								label: 'YouTube',
-								href: 'https://youtube.com/@TechnicalStudioDeveloper',
-              },
-							{
-								label: 'Discord',
-								href: 'https://discord.gg/xG7Y6buT',
-              },
-							{
-								label: 'Facebook',
-								href: 'https://m.facebook.com/profile.php?id=100019763839969'
+							}
+						]
+					},
+ 					{
+ 						title: 'Community',
+ 						items: [
+ 							{
+ 								label: 'YouTube',
+ 								href: 'https://youtube.com/@TechnicalStudioDeveloper'
 							},
-            ],
-          },
-					{
-						title: 'More',
-						items: [
-							/*{
-								label: 'Blog',
-								to: '/blog',
-              },*/
+ 							{
+ 								label: 'Discord',
+ 								href: 'https://discord.gg/xG7Y6buT'
+							},
+ 							{
+ 								label: 'Facebook',
+ 								href: 'https://m.facebook.com/profile.php?id=100019763839969'
+							},
 							{
 								label: 'GitHub',
-								href: 'https://github.com/TS-Code-Editor',
-              },
-            ],
-          },
-        ],
-				copyright: `Copyright © ${new Date().getFullYear()} TS Code Editor, Inc. Built with Docusaurus.`,
-			},
-			prism: {
-				theme: lightCodeTheme,
-				darkTheme: darkCodeTheme,
-			},
-		}),
-};
+								href: 'https://github.com/TS-Code-Editor'
+							}
+						]
+					}
+				],
+ 				copyright: `Copyright © ${new Date().getFullYear()} TS Code Editor, Inc. Built with Docusaurus.`
+ 			},
+ 			prism: {
+ 				theme: lightCodeTheme,
+ 				darkTheme: darkCodeTheme,
+ 			}
 
-module.exports = config;
+ 		})
+ };
+
+ module.exports = config;
